3f9694132673edab1f4c94f7dd6eda960b86d355
Lucas Fiuza <lucex109@gmail.com>
2024-10-29 23:36:47 -0300

    s

--- NuvemBack/index.js
+++ NuvemBack/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-//teste2push2345*dnbcdcaslacdte
+//teste2push2345*dnbcdcaslacdtes
 const { json } = require("express");
 const express = require("express");
 const cors = require("cors")
