14db85de33f6d8b27592496dec701cfbe41b31cf
LuEx10 <lucex109@gmail.com>
2024-10-30 08:38:10 -0300

    s

--- NuvemBack/index.js
+++ NuvemBack/index.js
@@ -1,5 +1,5 @@
 require("dotenv").config();
-//teste2push2345*dnbcdcaslacdtes
+//teste2push2345*dnbcdcaslacdtess
 const { json } = require("express");
 const express = require("express");
 const cors = require("cors")
